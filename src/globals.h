02f12dff054c5dd93d8b013048fb235e53eb61b6
Cherry-Trees <147207363+Cherry-Trees@users.noreply.github.com>
2024-03-06 03:20:33 -0600

    Update globals.h

--- src/globals.h
+++ src/globals.h
@@ -1,15 +1,16 @@
 #pragma once
 #include <math.h>
+#include "Mat44.h"
 
-const int W = 40;
-const int H = 20;
-const float ASPECT_RATIO = (float)H / (float)W;
+#define W 40
+#define H 20
+#define ASPECT_RATIO (float)H / (float)W
 
-const float Z_NEAR = 0.1f;
-const float Z_FAR = 100.f;
-const float FOV = 3.1415926535897932f / 2.f;
-const float FOCAL_LENGTH = 1.f / tan(FOV / 2.f);
-const float DISTANCE_FROM_CAMERA = 1.5f;
-const float INIT_TRANSLATION = 1.f;
-
-const char PROJECTED_CHAR[6] = {' ', '.', '~', '+', '$', '#'};
+#define Z_NEAR 0.1f
+#define Z_FAR 100.f
+#define FOV 3.1415926535897932f / 2.f
+#define HALF_FOV FOV / 2.f
+#define FOCAL_LENGTH 1.f / tanf(HALF_FOV)
+#define DISTANCE_FROM_CAMERA 1.f
+#define INIT_TRANSLATION 1.f
+#define N_TRIS 12

317699fc3d8ee2be122a280368f22185edbadcce
Cherry-Trees <147207363+Cherry-Trees@users.noreply.github.com>
2024-03-06 16:53:08 -0600

    Update globals.h

--- src/globals.h
+++ src/globals.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <math.h>
+#include "Mat44.h"
 
 #define W 40
 #define H 20
@@ -7,9 +8,10 @@
 
 #define Z_NEAR 0.1f
 #define Z_FAR 100.f
+#define X_SCALE 1.7f
 #define FOV 3.1415926535897932f / 2.f
 #define HALF_FOV FOV / 2.f
 #define FOCAL_LENGTH 1.f / tanf(HALF_FOV)
-#define DISTANCE_FROM_CAMERA 1.f
+#define DISTANCE_FROM_CAMERA 1.5f
 #define INIT_TRANSLATION 1.f
 #define N_TRIS 12
